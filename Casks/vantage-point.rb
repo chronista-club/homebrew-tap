ae8d1d0f3b031bf3d62f06008f97a0815b658771
Mako <74936+mako-357@users.noreply.github.com>
2026-07-10 11:32:00 +0900

    chore(cask): vantage-point 0.37.0

--- Casks/vantage-point.rb
+++ Casks/vantage-point.rb
@@ -1,6 +1,6 @@
 cask "vantage-point" do
-  version "0.36.0"
-  sha256 "82efd96d982f777d9234c8eab6e1410c8b99c685de6373e7c98fc0e40b4a3649"
+  version "0.37.0"
+  sha256 "46c29594d3ddda05cf56ab897a0c72061f3e1c61ca93fe3cd658ec93304ad643"
 
   url "https://github.com/chronista-club/vantage-point/releases/download/v#{version}/VantagePoint-#{version}-arm64.dmg"
   name "Vantage Point"
